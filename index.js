c7ec9151c0264990eae5f1009f0d546ac0264f89
Gafarov <pgafarov@dataarmor.ru>
2018-01-23 08:48:54 +0300

    set main

---
+++ index.js
@@ -0,0 +1,17 @@
+import StorageInterface from './src/StorageInterface'
+import StorageLocal from './src/StorageLocal'
+import StorageSimple from './src/StorageSimple'
+export {
+  /**
+   * @type {StorageInterface}
+   */
+  StorageInterface,
+  /**
+   * @type {StorageLocal}
+   */
+  StorageLocal,
+  /**
+   * @type {StorageSimple}
+   */
+  StorageSimple
+}
